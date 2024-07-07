4503a3522fc1e308863d30ce9dc14a5d0aca89bd
rfmineguy <rfmineguy@gmail.com>
2024-07-06 18:01:02 -0700

    1.20 fabric

---
+++ 1.20-fabric/src/client/java/io/github/rfmineguy/EnhancedFarmingClient.java
@@ -0,0 +1,10 @@
+package io.github.rfmineguy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnhancedFarmingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
